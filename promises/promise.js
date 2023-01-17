44b57ca72690ac4562c4a6268b1e2b5b58f36f8f
syed hussain <syedhussain@syeds-mbp.lan>
2023-01-16 22:18:30 -0500

    added promises images

--- promises/promise.js
+++ promises/promise.js
@@ -80,3 +80,67 @@ promise.catch((error) => {
       render();
     });
   
+    //<<<<FIVE>>//
+
+const promise1 = Promise.resolve(3);
+const promise2 = 42;
+const promise3 = new Promise((resolve, reject) => {
+  setTimeout(resolve, 100, 'foo');
+});
+
+//Promise.all takes multiple promises as input and reolved 
+
+Promise.all([promise1, promise2, promise3]).then((values) => {
+  console.log(values);
+});
+// Expected output: 
+// Array [3, 42, "foo"]
+
+
+//<<<PROMISE.Resolve>>>////
+const promise1 = Promise.resolve(123);
+
+promise1.then((value) => {
+  console.log(value);
+
+});
+
+// Expected output: 123
+
+////<<<<Promise.Reject>>>///
+
+function resolved(result) {
+    console.log('Resolved');
+  }
+  
+  function rejected(result) {
+    console.error(result);
+  }
+  
+  Promise.reject(new Error('fail')).then(resolved, rejected);
+ 
+  // Expected output: Error: fail
+  
+  ///<<<Promise.Catch>>>>>///
+
+  const promise1 = new Promise((resolve, reject) => {
+    throw new Error('Uh-oh!');
+  });
+  
+  promise1.catch((error) => {
+    console.error(error);
+  });
+
+  // Expected output: Error: Uh-oh!
+  
+  ////<<<<Promise.then>>>>//////
+
+  const promise1 = new Promise((resolve, reject) => {
+    resolve('Success!');
+  });
+  
+  promise1.then((value) => {
+    console.log(value);
+  });
+  
+// Expected output: "Success!"
